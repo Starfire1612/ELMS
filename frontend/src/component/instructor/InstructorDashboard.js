0b09daf9c3df5524ee3a0d2280bd54aa2f550081
PirAte <iam.anurag@gmail.com>
2022-07-26 01:50:11 +0530

    Updated Instructor Dashboard

--- frontend/src/component/instructor/InstructorDashboard.js
+++ frontend/src/component/instructor/InstructorDashboard.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import InstructorNavbar from "./InstructorNavbar";
 import "../../styles/InstructorDashboard.css";
 import { Button, FormControl } from "react-bootstrap";
 import { Search } from "react-bootstrap-icons";
+import StaticDashboardComponents from "./StaticDashboardComponents";
+import { ClipLoader } from "react-spinners";
+import { LOADING_COLOR } from "../../utils/constants";
+import Courses from "../Courses";
+import { dummyCourses } from "../dummydata/dummyCourses.js";
 
 function InstructorDashboard({ handleLogout, userData }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [courseList, setCourseList] = useState(dummyCourses);
+
+  const fetchCourse = async () => {
+    //fetch instructor courses using id from userData and set courseList
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchCourse();
+    setIsLoading(false);
+  }, []);
+
   return (
     <div>
       <InstructorNavbar handleLogout={handleLogout} userData={userData} />
@@ -32,7 +50,7 @@ function InstructorDashboard({ handleLogout, userData }) {
               </Button>
             </div>
             <Button className="type-3">
-              <p className="filter">filter</p>
+              <p className="filter m-0">filter</p>
             </Button>
           </div>
           <Button className="type-1 fw-bolder new-course-button-2">
@@ -41,6 +59,18 @@ function InstructorDashboard({ handleLogout, userData }) {
             </Link>
           </Button>
         </div>
+        {isLoading ? (
+          <ClipLoader className="d-block mx-auto mt-5" color={LOADING_COLOR} />
+        ) : courseList.length ? (
+          <Courses courses={courseList} />
+        ) : (
+          <>
+            <div className="courses-list text-center my-5">
+              You do not have published any courses yet.
+            </div>
+            <StaticDashboardComponents />
+          </>
+        )}
       </div>
     </div>
   );
