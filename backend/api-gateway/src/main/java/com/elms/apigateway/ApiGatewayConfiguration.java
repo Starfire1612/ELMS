a4fea0dcf02a495d65598cf89aab3fe0a95884bc
Starfire1612 <radhikashah1612@gmail.com>
2022-08-04 19:53:15 +0530

    integrated api gateway

--- backend/api-gateway/src/main/java/com/elms/apigateway/ApiGatewayConfiguration.java
+++ backend/api-gateway/src/main/java/com/elms/apigateway/ApiGatewayConfiguration.java
@@ -1,8 +1,26 @@
 package com.elms.apigateway;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ApiGatewayConfiguration {
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p.path("/authenticate/**").uri("lb://authentication-service"))
+				.route(p -> p.path("/greetings/**").uri("lb://authentication-service"))
+				.route(p -> p.path("/authorize/**").uri("lb://authentication-service"))
+				.route(p -> p.path("/forgot-password/**").uri("lb://authentication-service"))
+				.route(p -> p.path("/course/**").uri("lb://database-service"))
+				.route(p -> p.path("/student/**").uri("lb://database-service"))
+				.route(p -> p.path("/instructor/**").uri("lb://database-service"))
+				.route(p -> p.path("/all-payments/**").uri("lb://database-service"))
+				.route(p -> p.path("/register-user/**").uri("lb://registration-service"))
+				.route(p -> p.path("/registertration/**").uri("lb://registration-service"))
+				.build();
+	}
 
 }
